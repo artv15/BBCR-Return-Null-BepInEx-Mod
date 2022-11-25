f6e169780039085e6f8a4cf688ab9853a2494ba8
Treeshold <Treeshold@treesholdapi.ml>
2022-11-25 18:44:00 +0300

    FILES

---
+++ BBCR Return Null Mod/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace BBCR_Return_Null_Mod
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony patcher = new Harmony("TheFallenProject.NullMod.Patcher");
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            patcher.PatchAll(typeof(MainMenuPatcher));
+            Logger.LogInfo("All patches have been applied!");
+        }
+    }
+
+    internal class MainMenuPatcher
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MainMenu), "Start")]
+        public static void Start()
+        {
+            var lgr = new ManualLogSource("MenuPatch");
+            lgr.LogMessage("Tried to update teh values!");
+            Singleton<PlayerFileManager>.Instance.flags[4] = false;
+            Singleton<PlayerFileManager>.Instance.flags[5] = false;
+            Singleton<PlayerFileManager>.Instance.flags[0] = true;
+        }
+    }
+}
